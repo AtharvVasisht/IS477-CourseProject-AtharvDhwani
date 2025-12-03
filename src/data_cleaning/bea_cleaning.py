87455978e9e1f00abf15938eea3eacb1b8614ac7
atharv-vasisht <atharvv3@illinois.edu>
2025-12-03 16:21:15 -0600

    adjusting bea_cleaning file to make data ready for integration

--- src/data_cleaning/bea_cleaning.py
+++ src/data_cleaning/bea_cleaning.py
@@ -1,7 +1,7 @@
 ''' The BEA data which was originally downloaded as an excel file, was already converted to a csv file in the 
 acquisition part of our project. When examining the csv converted file, we already identified that there were
 no null values present in the data - in other words, the data is already clean. This script will do a few things: it will first 
-organize this csv into sizerank to match the zillow data, and then it will be stored as a cleaned csv to the data/processed folder.
+organize this csv into sizerank to match the zillow data (based on Zillow's 2024 metropolitan area size rankings), and then it will be stored as a cleaned csv to the data/processed folder.
 '''
 
 # imports (added to requirements.txt)
@@ -13,17 +13,27 @@ from fuzzywuzzy import process, fuzz
 
 # Load bea_csv from raw/BEA_PersonalIncome_Data_converted.csv
 bea_csv = Path(__file__).resolve().parent.parent.parent / "data" / "raw" / "BEA_PersonalIncome_Data_converted.csv"
-unadjusted_bea = pd.read_csv(bea_csv)
+unadjusted_bea = pd.read_csv(bea_csv, skiprows = 5) # skipping the unnecessary header rows at the top of the file
 
 # Load zillow_cleaned data to match cities
 zillow_cleaned_path = Path(__file__).resolve().parent.parent.parent / "data" / "processed" / "zillow_cleaned.csv"
 zillow_cleaned = pd.read_csv(zillow_cleaned_path)
+
+# we must standardize column names to make sure there's no breakge in our organization function below
+unadjusted_bea.columns = unadjusted_bea.columns.str.strip()
+zillow_cleaned.columns = zillow_cleaned.columns.str.strip()
 
 # utilize the zillow_cleaned data to organize the bea into only necessary cities by size rank
 def organize_bea(unadjusted_bea, zillow_cleaned):
     # we first need to store the zillow_cleaned and get the cities within "RegionName" column into a list to iterate through
     zillow_cities = zillow_cleaned["RegionName"].tolist()
 
+    # now, we need to iterate through the zillow_cities list and find the closest fuzzy match between the city name and the unadjusted_bea["RegionName"] column
+    # BEA uses a similar "City, State" format as Zillow, so we don't need to worry about splitting the city and state names apart and can just focus on finding approximate matches
+    # logic for reorganizing the bea data to match the zillow data: 
+    # itereate through zillow_data
+
+    '''
     # now we need to temporarily split the unadjusted_bea into city and state (right now the format is "City, State")
     unadjusted_bea["City"] = unadjusted_bea["RegionName"].str.split(",").str[0]
     unadjusted_bea["State"] = unadjusted_bea["RegionName"].str.split(",").str[1]
@@ -40,7 +50,7 @@ def organize_bea(unadjusted_bea, zillow_cleaned):
     
     # now we need to filter the unadjusted_bea dataframe to only include the cities that are in the matched_bea_cities list
     adjusted_bea = unadjusted_bea[unadjusted_bea["City"].isin(matched_bea_cities)].copy()
-    
+    '''
     return adjusted_bea
 
 # execute this function and store the result into bea_df
